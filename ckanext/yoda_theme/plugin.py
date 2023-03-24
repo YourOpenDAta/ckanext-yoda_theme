110b4464272c60fe30e81c1bf3173b467f1a9b83
pablouxo <pablo.uxo.castillo@gmail.com>
2023-03-24 10:51:41 +0100

    english internationalization added

--- ckanext/yoda_theme/plugin.py
+++ ckanext/yoda_theme/plugin.py
@@ -1,14 +1,18 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 
 
-class YodaThemePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+from ckan.plugins import  ITranslation
+from ckan.plugins.interfaces import IConfigurer
+
+class YodaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
+    plugins.implements(ITranslation)
+    plugins.implements(IConfigurer)
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'yoda_theme')
+        toolkit.add_resource('fanstatic', 'yoda_theme')
